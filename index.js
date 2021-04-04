93c1847f033256203c9a4c1e47822ebcecc241e3
Chris <cmlentz76@gmail.com>
2021-04-04 06:28:57 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  let costReply;
+  if (distance > 2500) {
+    costReply = "No can do.";
+  } else if (distance > 2000) {
+      costReply = "I will gladly take your thirty bucks.";
+  } else if (distance < 401) {
+    costReply = "This one is on me!";
+  }
+  return costReply;
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity (city){
   // Write your code here!
+  const reply = city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+  return reply;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+      break;
+    case 'not as generous':
+      return 'Thank you.';
+      break;
+    default:
+      return 'Bye.';
+      break;
+  }
 }
